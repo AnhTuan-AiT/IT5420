974f0e63f6657fc94183e82442d8d365ed50d056
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-05-13 22:30:14 +0700

    integrate views

--- src/App.js
+++ src/App.js
@@ -1,7 +1,45 @@
+import { CssBaseline, LinearProgress } from "@material-ui/core";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import { createBrowserHistory } from "history";
+import { Suspense } from "react";
+import { Router } from "react-router-dom";
 import "./App.css";
+import Layout from "./components/layout/Layout";
+import Routes from "./routers/routes";
+
+export const history = createBrowserHistory();
+
+const theme = createMuiTheme({
+  typography: {
+    fontFamily: `-apple-system, "Segoe UI", BlinkMacSystemFont, "Roboto", "Oxygen",
+    "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue",
+    sans-serif`,
+  },
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        body: {
+          height: "100%",
+          backgroundColor: "#fff",
+        },
+      },
+    },
+  },
+});
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <MuiThemeProvider theme={theme}>
+      <CssBaseline />
+      <Router history={history}>
+        <Suspense fallback={<LinearProgress />}>
+          <Layout>
+            <Routes />
+          </Layout>
+        </Suspense>
+      </Router>
+    </MuiThemeProvider>
+  );
 }
 
 export default App;
