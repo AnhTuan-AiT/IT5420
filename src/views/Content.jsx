7745e59a1cf604a1eff34d354cddac9c6f4501b7
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-05-13 22:30:52 +0700

    add Content view

---
+++ src/views/Content.jsx
@@ -0,0 +1,125 @@
+import { Divider, Grid, Link } from "@material-ui/core";
+import { orange } from "@material-ui/core/colors";
+import { makeStyles } from "@material-ui/core/styles";
+import React, { useState } from "react";
+import Paper from "../components/Paper";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.paper,
+  },
+  div: {
+    maxWidth: 330,
+    width: "100%",
+  },
+  div1: {
+    maxWidth: 625,
+    paddingBottom: 32,
+    width: "100%",
+  },
+  divider: {
+    margin: "12px 0px",
+  },
+  cate: {
+    position: "relative",
+    display: "inline-block",
+    fontSize: 22,
+    padding: "4px 12px 12px",
+    textTransform: "uppercase",
+    color: orange[500],
+    textDecoration: "none",
+    "&:hover": { textDecoration: "none", color: orange[900] },
+    "&::before": {
+      content: '""',
+      display: "inline-block",
+      width: 2,
+      height: "calc(100% - 16px)",
+      position: "absolute",
+      top: 4,
+      left: 0,
+      background: orange[500],
+    },
+    "&:hover:before": {
+      background: orange[900],
+    },
+  },
+  mdWrapper: {
+    padding: "32px 0px",
+  },
+  mdWrapperNonPadding: {
+    padding: 0,
+  },
+}));
+
+function Hot({ hotNews }) {
+  const classes = useStyles();
+  const [papers, setPapers] = useState();
+
+  const title = ["Thế giới", "Xã hội", "Kinh tế", "Thể thao"];
+
+  return (
+    <Grid container spacing={2}>
+      <Grid item className={classes.div1}>
+        {hotNews ? <Paper lg paper={papers?.hot} /> : null}
+        {papers ? (
+          title.map((cate) => (
+            <>
+              <Link href="#" className={classes.cate}>
+                {cate}
+              </Link>
+              {Array(5)
+                .fill(0)
+                .map((ele, index) => (
+                  <>
+                    <Paper
+                      md
+                      title="Tổng Bí thư Nguyễn Phú Trọng điện đàm với Bí thư thứ nhất Đảng Cộng sản Cuba"
+                    />
+                    {index === 4 ? null : (
+                      <Divider
+                        variant="fullWidth"
+                        className={classes.divider}
+                      />
+                    )}
+                  </>
+                ))}
+            </>
+          ))
+        ) : hotNews ? (
+          <div className={classes.mdWrapper}>
+            <Paper md />
+          </div>
+        ) : (
+          Array(4)
+            .fill(0)
+            .map((ele, index) => (
+              <>
+                <Paper md />
+                <Divider variant="fullWidth" className={classes.divider} />
+              </>
+            ))
+        )}
+      </Grid>
+      <Grid item className={classes.div}>
+        {papers
+          ? null
+          : Array(5)
+              .fill(0)
+              .map((ele, index) => (
+                <>
+                  {index === 0 ? null : (
+                    <Divider variant="fullWidth" className={classes.divider} />
+                  )}
+                  <Paper
+                    sm
+                    title="Tổng Bí thư Nguyễn Phú Trọng điện đàm với Bí thư thứ nhất Đảng Cộng sản Cuba"
+                  />
+                </>
+              ))}
+      </Grid>
+    </Grid>
+  );
+}
+
+export default Hot;
