7107b2907283cbda8ee7d3fe09c18c4554cc1326
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-05-22 22:56:25 +0700

    config route for new tabs

--- src/routers/routes.js
+++ src/routers/routes.js
@@ -2,7 +2,9 @@ import React, { lazy } from "react";
 import { Redirect, Route, Switch } from "react-router";
 
 const Content = lazy(() => import("../views/Content"));
-const CateContent = lazy(() => import("../views/CateContent"));
+const CateContent = lazy(() => import("../views/Cate"));
+const SearchContent = lazy(() => import("../views/SearchContent"));
+const Similar = lazy(() => import("../views/Similar"));
 
 function Routes() {
   return (
@@ -12,33 +14,43 @@ function Routes() {
         path="/"
         exact
       />
+
       <Route
-        render={(props) => <CateContent {...props} />}
-        path="/news"
-        exact
-      />
-      <Route
-        render={(props) => <CateContent {...props} />}
+        render={(props) => <CateContent {...props} hotNews />}
         path="/world"
         exact
       />
+
       <Route
-        render={(props) => <CateContent {...props} />}
-        path="/society"
+        render={(props) => <CateContent {...props} hotNews />}
+        path="/entertainment"
         exact
       />
+
       <Route
-        render={(props) => <CateContent {...props} />}
+        render={(props) => <CateContent {...props} hotNews />}
+        path="/education"
+        exact
+      />
+
+      <Route
+        render={(props) => <CateContent {...props} hotNews />}
         path="/economy"
         exact
       />
+
       <Route
-        render={(props) => <CateContent {...props} />}
+        render={(props) => <CateContent {...props} hotNews />}
         path="/sport"
         exact
       />
-      <Route component={Content} path="/search/:keyword?" />
-      <Route component={Content} path="/news/paper/:paperName" />
+
+      <Route component={SearchContent} path="/search/:keyword?" />
+
+      <Route component={Similar} path="/relevant/:id" />
+
+      <Route component={SearchContent} path="/news/paper/:paperName" />
+
       <Redirect push to="/" />
     </Switch>
   );
