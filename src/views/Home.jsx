036867876a7ac5f71eebfc22bfdb5e542fbbc8a5
Lê Anh Tuấn <anhtuan0126104@gmail.com>
2021-05-23 11:55:18 +0700

    integrate API

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,13 @@
 import { Grid, Link } from "@material-ui/core";
 import { orange } from "@material-ui/core/colors";
 import { makeStyles } from "@material-ui/core/styles";
+import axios from "axios";
 import React, { Fragment, useEffect, useState } from "react";
-import { useParams } from "react-router";
-import request from "../api";
+import { Link as RouterLink } from "react-router-dom";
+import request, { paralelRequest } from "../api";
+import { tabs } from "../components/NavTabs";
+import News from "../components/News";
+import Pagination from "../components/Pagination";
 import Paper from "../components/Paper";
 import { StyledDivider } from "../components/StyledDivider";
 
@@ -23,10 +27,9 @@ const useStyles = makeStyles((theme) => ({
   },
   cate: {
     position: "relative",
-    zIndex: -1,
     display: "inline-block",
     fontSize: 22,
-    padding: "4px 12px 12px",
+    padding: "12px 12px",
     textTransform: "uppercase",
     color: orange[500],
     textDecoration: "none",
@@ -35,9 +38,9 @@ const useStyles = makeStyles((theme) => ({
       content: '""',
       display: "inline-block",
       width: 2,
-      height: "calc(100% - 16px)",
+      height: "calc(100% - 24px)",
       position: "absolute",
-      top: 4,
+      top: 12,
       left: 0,
       background: orange[500],
     },
@@ -55,41 +58,128 @@ const useStyles = makeStyles((theme) => ({
 
 function Content({ hotNews, location }) {
   const classes = useStyles();
-  const [papers, setPapers] = useState();
-  const params = useParams();
-
-  const title = ["Thế giới", "Xã hội", "Kinh tế", "Thể thao"];
 
   //
-  useEffect(() => {
-    let url = location.pathname.substring(1);
-    url = url === "" ? "news" : url;
-    if (params.paperName) url = `news/${params.paperName}`;
+  const [papers, setPapers] = useState();
+  const [catePapers, setCatePapers] = useState();
+  const [currPage, setCurrPage] = useState(0);
+  const [lastPage, setLastPage] = useState(false);
 
-    request("get", `${url}?offset=0&limit=20`, (res) => {
-      setPapers(res.data.content);
-    });
-  }, [location]);
+  //
+  const cate = ["Thế giới", "Kinh tế", "Giáo dục", "Giải trí", "Thể thao"];
+  const urls = cate.map((c) => `news/category?category=${c}&offset=0&limit=20`);
+
+  //
+  const onViewNextPage = () => {
+    if (currPage === 0) setCatePapers();
+    setCurrPage((currPage) => currPage + 1);
+    setPapers((prev) => ({
+      smList: prev.smList,
+    }));
+  };
+
+  const onViewPreviousPage = () => {
+    setCurrPage((currPage) => currPage - 1);
+    setPapers((prev) => ({
+      smList: prev.smList,
+    }));
+  };
+
+  // const getCatePapers = () => {
+  //   try {
+  //     Promise.all(
+  //       [...Array(5).keys()].map((idx) =>
+  //         axios.get(`${process.env.REACT_APP_API_ENDPOINT}${urls[idx]}`)
+  //       )
+  //     ).then((responses) => {
+  //       const data = [];
+
+  //       responses.forEach((res, idx) => {
+  //         data.push({ cate: cate[idx], data: res.data.content });
+  //       });
+
+  //       setCatePapers(data);
+  //     });
+  //   } catch (e) {
+  //     if (e.response) {
+  //       // The request was made and the server responded with a status code that falls out of the range of 2xx.
+  //       console.log(
+  //         "The request was made and the server responded with a status code that falls out of the range of 2xx",
+  //         e
+  //       );
+  //     } else if (e.request) {
+  //       // The request was made but no response was received
+  //       // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+  //       // http.ClientRequest in node.js
+  //       console.log(
+  //         "The request was made but no response was received",
+  //         e.request
+  //       );
+  //     } else {
+  //       // Something happened in setting up the request that triggered an Error.
+  //       console.log(
+  //         "Something happened in setting up the request that triggered an Error",
+  //         e.message
+  //       );
+  //     }
+
+  //     console.log("Request config", e.config);
+  //   }
+  // };
+
+  useEffect(() => {
+    if (currPage === 0) {
+      request("get", `news?offset=0&limit=20`, (res) => {
+        const data = res.data.content;
+        setPapers({
+          hot: data[0],
+          mdList: data.slice(1, 10),
+          smList: data.slice(10),
+        });
+      });
+
+      // getCatePapers();
+      paralelRequest(
+        [...Array(5).keys()].map((idx) =>
+          axios.get(`${process.env.REACT_APP_API_ENDPOINT}${urls[idx]}`)
+        ),
+        (responses) => {
+          const data = [];
+
+          responses.forEach((res, idx) => {
+            data.push({ cate: cate[idx], data: res.data.content });
+          });
+
+          setCatePapers(data);
+        }
+      );
+    } else {
+      request("get", `news?offset=${currPage}&limit=50`, (res) => {
+        const data = res.data.content;
+        setPapers((prev) => ({ mdList: data, smList: prev.smList }));
+      });
+    }
+  }, [location, currPage]);
 
   return (
     <Grid container spacing={2} justify="center">
       <Grid item className={classes.div1}>
-        {hotNews ? <Paper lg paper={papers?.hot} /> : null}
-        {papers ? (
-          title.map((cate) => (
-            <Fragment key={cate}>
-              <Link href="#" className={classes.cate}>
-                {cate}
-              </Link>
-              {papers.map((p, index) => (
-                <Fragment key={p.id}>
-                  <Paper md paper={p} />
-                  {index === papers.length - 1 ? null : <StyledDivider />}
-                </Fragment>
-              ))}
+        {hotNews && currPage === 0 ? (
+          <>
+            <Paper lg paper={papers?.hot} />
+            {papers ? <StyledDivider /> : null}
+          </>
+        ) : null}
+
+        {/* News */}
+        {papers?.mdList ? (
+          papers.mdList.map((p, index) => (
+            <Fragment key={p.id}>
+              <Paper md paper={p} />
+              {index === papers.mdList.length - 1 ? null : <StyledDivider />}
             </Fragment>
           ))
-        ) : hotNews ? ( // Loading screen.
+        ) : hotNews && currPage === 0 ? ( // Loading screen.
           <div className={classes.mdWrapper}>
             <Paper md />
             <StyledDivider />
@@ -98,34 +188,50 @@ function Content({ hotNews, location }) {
         ) : (
           Array(4)
             .fill(0)
-            .map((index) => (
+            .map((ele, index) => (
               <Fragment key={index}>
                 <Paper md />
                 <StyledDivider />
               </Fragment>
             ))
         )}
-      </Grid>
 
-      {/* abc */}
-      <Grid item className={classes.div}>
-        {papers
-          ? papers.map((p, index) => (
-              <Fragment key={p.id}>
-                {index === 0 ? null : <StyledDivider />}
-                <Paper sm paper={p} />
+        {/* Cate */}
+        {catePapers
+          ? catePapers.map((papers) => (
+              <Fragment key={papers.cate}>
+                <Link
+                  component={RouterLink}
+                  to={tabs[papers.cate]}
+                  className={classes.cate}
+                >
+                  {papers.cate}
+                </Link>
+                {papers.data.map((p, index) => (
+                  <Fragment key={p.id}>
+                    <Paper md paper={p} />
+                    {index === papers.data.length - 1 ? null : (
+                      <StyledDivider />
+                    )}
+                  </Fragment>
+                ))}
               </Fragment>
             ))
-          : // Loading screen.
-            Array(5)
-              .fill(0)
-              .map((ele, index) => (
-                <Fragment key={index}>
-                  {index === 0 ? null : <StyledDivider />}
-                  <Paper sm />
-                </Fragment>
-              ))}
+          : null}
       </Grid>
+
+      {/* News */}
+      <News news={papers?.smList} />
+
+      {/* Pagination */}
+      <Pagination
+        show={papers?.mdList}
+        currPage={currPage}
+        lastPage={lastPage}
+        onPreviousPage={onViewPreviousPage}
+        onNextPage={onViewNextPage}
+      />
+      {/* papers?.mdList */}
     </Grid>
   );
 }
